7a2c3a404cc81ca790e7d528aca56a4733fac22e
Matt <amsellem.matt@gmail.com>
2020-02-04 08:20:36 +0100

    Cryptage Decryptage complètement fonctionnel

--- Code C/Code C/crypt_decrypt.c
+++ Code C/Code C/crypt_decrypt.c
@@ -2,13 +2,12 @@
 
 void RSAcrypt(unsigned char *msg, uint64 *cryptedMsg, rsaKey_t pubKey){
     for (int i = 0; i < strlen(msg); i++){
-        cryptedMsg[i] = (uint64)msg[i]*(pubKey.E%pubKey.N);
+        cryptedMsg[i] = puissance_mod_n(msg[i], pubKey.E, pubKey.N);
     }
 }
 
 void RSAdecrypt(unsigned char *msg, uint64 *cryptedMsg, rsaKey_t privKey){
     for (int i = 0; cryptedMsg[i] != '\0'; i++){
-        unsigned char c = cryptedMsg[i]*(privKey.E%privKey.N);
-        msg[i] = c;
+        msg[i] = puissance_mod_n(cryptedMsg[i], privKey.E, privKey.N);
     }
 }
