ddeb935738ce265dbecd396950f702e9468aadb7
Matt <amsellem.matt@gmail.com>
2020-03-03 18:38:00 +0100

    derniere version pour Damien

--- Code C/Code C/rsa_file_char.c
+++ Code C/Code C/rsa_file_char.c
@@ -42,17 +42,21 @@ void RSAcryptFile(char *inFilename, char *outFilename, rsaKey_t pubKey, int *out
     int i = 0;
     size_t _output_length = (size_t)*output_length;
     uint64 *cryptedBuffer = (uint64 *)malloc(MAX_BUFFER*sizeof(uint64));
+    printf("\nPhase cryptage : \n\n");
     do{
         fgets(buffer, MAX_BUFFER, fichier);
         printf("phrase : %s\n", buffer);
-        RSAcrypt((unsigned char*)buffer, cryptedBuffer, pubKey);
-        for (i = 0; cryptedBuffer[i] != '\0'; i++){
-            fwrite(base64_encode((const uchar*)&cryptedBuffer[i], sizeof(cryptedBuffer[i]), &_output_length), 1, sizeof(base64_encode((const uchar*)&cryptedBuffer[i], sizeof(cryptedBuffer[i]), &_output_length)), fichier2);
+        for (i = 0; buffer[i] != '\0'; i++){
+            RSAcrypt((unsigned char*)&buffer[i], &cryptedBuffer[i], pubKey);
+            printf("%d : %lu :",i, &cryptedBuffer[i]);
+            char * buffer = base64_encode((const uchar*)&cryptedBuffer[i], sizeof(cryptedBuffer[i]), &_output_length);
+            printf("%s\n", buffer);
+            fwrite(buffer, 1, 12, fichier2);
         }
-        free(buffer);
-        free(cryptedBuffer);
+        //free(buffer);
+        //free(cryptedBuffer);
+        *output_length += _output_length;
     }while (!feof(fichier));
-    *output_length = _output_length;
     fclose(fichier);
     fclose(fichier2);
     fseek(fichier, 0, SEEK_SET);
@@ -75,12 +79,13 @@ void RSAunCryptFile(char *inFilename,char *outFilename,rsaKey_t privKey, int len
     do{
         fread(buffer, 1, _length, fichier);
         printf("%s\n", buffer);
-        uint64 *cryptedBuffer = base64_decode((const char *)buffer,_length,&_length);
-        //printf("%d\n", cryptedBuffer);
-        // RSAdecrypt(&cUnCrypt, cryptedBuffer, privKey);
+        uint64 *cryptedBuffer = (uint64 *)base64_decode((const char *)buffer,_length,&_length);
+        printf("%lu\n", cryptedBuffer);
+        //RSAdecrypt(&cUnCrypt, cryptedBuffer, privKey);
         cUnCrypt = puissance_mod_n(cryptedBuffer,privKey.E,privKey.N);
         //fprintf(fichier2, "%c\n", (char)cUnCrypt);
-        fwrite(&cUnCrypt, 1, 1, fichier2);
+        int * cDecrypt = &cUnCrypt;
+        fwrite(cDecrypt, 1, 1, fichier2);
         free(cryptedBuffer);
     }while (!feof(fichier));
     fclose(fichier);
