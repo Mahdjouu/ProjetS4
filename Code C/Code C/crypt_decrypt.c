b8cbc5e59d87b1bea6a2e2504d80b8377a2a2374
Matt <amsellem.matt@gmail.com>
2020-03-09 18:18:11 +0100

    dernier commit

--- Code C/Code C/crypt_decrypt.c
+++ Code C/Code C/crypt_decrypt.c
@@ -1,14 +1,15 @@
 #include "rsa_header.h"
 
 void RSAcrypt(unsigned char *msg, uint64 *cryptedMsg, rsaKey_t pubKey){
-    for (int i = 0; i < strlen(msg); i++){
+    for (int i = 0; msg[i] != '\0'; i++){
         cryptedMsg[i] = puissance_mod_n(msg[i], pubKey.E, pubKey.N);
     }
+    *(cryptedMsg+1) = '\0';
 }
 
 void RSAdecrypt(unsigned char *msg, uint64 *cryptedMsg, rsaKey_t privKey){
     for (int i = 0; cryptedMsg[i] != '\0'; i++){
         msg[i] = (char)puissance_mod_n(cryptedMsg[i], privKey.E, privKey.N);
     }
-    
+    *(msg+1) = '\0';
 }
